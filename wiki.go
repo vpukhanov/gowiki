673611f868b7748133502dbe64b9ad52c5df3216
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2018-03-07 22:20:39 +0300

    Adds a handler to make web root redirect to /view/FrontPage

--- wiki.go
+++ wiki.go
@@ -36,6 +36,10 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusMovedPermanently)
+}
+
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
@@ -78,6 +82,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 func main() {
+	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
